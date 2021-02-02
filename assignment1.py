e8ffd39f0ce2eb1829c9b420aedc0781b0fe65f6
torkilsg <torkilsg@stud.ntnu.no>
2021-02-02 15:57:41 +0100

    oppdatering av funksjonsnavn

--- assignment1.py
+++ assignment1.py
@@ -9,7 +9,10 @@ def Team_SetName(team, name):
     team[0] = name
 
 def Team_SetTrigram(team, trigram):
-    team[1] = trigram
+    if(len(trigram)>3):
+        print("Trigram cant be more than three characters.")
+    else:
+        team[1] = trigram
 
 def Team_GetTrigram(team):
     return team[1]
@@ -17,36 +20,36 @@ def Team_GetTrigram(team):
 def Team_SetNumGamesWon(team, gamesWon):
         team[2] = gamesWon
     
-def Team_getNumGamesWon(team):
+def Team_GetNumGamesWon(team):
     return team[2]
 
 def Team_SetNumGamesLost(team, gamesLost):
     team[3] = gamesLost
 
-def Team_getNumGamesDraw(team):
+def Team_GetNumGamesDraw(team):
     return team[4]
 
 def Team_SetNumGamesDraw(team, gamesDraw):
     team[4] = gamesDraw
 
 def Team_TotalPoints(team):
-    totalPoints = Team_getNumGamesWon(team) * 3 + Team_getNumGamesDraw(team) 
+    totalPoints = Team_GetNumGamesWon(team) * 3 + Team_GetNumGamesDraw(team) 
     return totalPoints
 
-def Team_setNumberOfGoalsFor(team, numberOfGoalsFor):
-    team[5] = numberOfGoalsFor
+def Team_SetNumOfGoalsFor(team, numOfGoalsFor):
+    team[5] = numOfGoalsFor
 
-def Team_getNumberOfGoalsFor(team):
+def Team_GetNumOfGoalsFor(team):
     return team[5]
 
-def Team_setNumberOfGoalsAgainst(team, numberOfGoalsAgainst):
-    team[6] = numberOfGoalsAgainst
+def Team_SetNumOfGoalsAgainst(team, numOfGoalsAgainst):
+    team[6] = numOfGoalsAgainst
 
-def Team_getNumberOfGoalsAgainst(team):
+def Team_GetNumOfGoalsAgainst(team):
     return team[6]
 
 def Team_goalDifference(team):
-    goalDifference = Team_getNumberOfGoalsFor(team) - Team_getNumberOfGoalsAgainst(team)
+    goalDifference = Team_GetNumOfGoalsFor(team) - Team_GetNumOfGoalsAgainst(team)
     return goalDifference
 
 
@@ -55,12 +58,18 @@ def Team_goalDifference(team):
 rosenborg = Team_New("Rosenborg", "RBK", 0 , 0, 0, 0, 0)
 Team_GetName(rosenborg)
 Team_GetTrigram(rosenborg)
-Team_SetNumGamesWon(rosenborg, 10).
+Team_SetNumGamesWon(rosenborg, 10)
+Team_SetNumGamesDraw(rosenborg, 8)
+Team_SetNumOfGoalsFor(rosenborg, 20)
+Team_SetNumOfGoalsAgainst(rosenborg, 5)
+Team_SetTrigram(rosenborg, "RBBK")
 print(Team_GetName(rosenborg))
 print(Team_GetTrigram(rosenborg))
-print(Team_getNumGamesWon(rosenborg))
-print(Team_getNumGamesDraw(rosenborg))
-print(Team_getNumberOfGoalsFor(rosenborg))
-print(Team_getNumberOfGoalsAgainst(rosenborg))
+print(Team_GetNumGamesWon(rosenborg))
+print(Team_GetNumGamesDraw(rosenborg))
+print(Team_GetNumOfGoalsFor(rosenborg))
+print(Team_GetNumOfGoalsAgainst(rosenborg))
+print(Team_goalDifference(rosenborg))
+print(Team_TotalPoints(rosenborg))
 
 
