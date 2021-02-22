71c9c90c41beeb0300e6ee30227df325aa387983
torkilsg <torkilsg@stud.ntnu.no>
2021-02-22 19:58:01 +0100

    edge.py update

--- assignment2/Edge.py
+++ assignment2/Edge.py
@@ -1,1 +1,15 @@
 
+
+class Edge(node1, node2):
+
+    def __init__(self, edge, node1, node2):
+        self.edge = [node1, node2]
+
+
+    def setEdge(self, edge, node1, node2):
+        self.edge = [node1, node2]
+    
+    def getEdge(self):
+        return self.edge
+    
+    
