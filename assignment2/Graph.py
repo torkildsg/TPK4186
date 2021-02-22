6a4281c1c1ba61c6ca8f5fbdd7cffdef89c95d7f
torkilsg <torkilsg@stud.ntnu.no>
2021-02-23 00:05:18 +0100

    graph.py update

--- assignment2/Graph.py
+++ assignment2/Graph.py
@@ -1,1 +1,12 @@
 
+class Graph():
+
+    def __init__(self):
+        self.nodes = dict()
+        self.edges = []
+    
+    def addNode(self, nodeName):
+        newNode = Node(nodeName)
+        thisNodesEdges = newNode.getEdgeList()
+        self.edges.append(thisNodesEdges)
+        self.nodes[nodeName] = thisNodesEdges
--- assignment2/Node.py
+++ assignment2/Node.py
@@ -1,4 +1,20 @@
 
 
 class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.edgeList = []
+    
+    def setName(self, name):
+        self.name = name
+    
+    def getName(self):
+        return self.name
+
+    def addEdge(self, edge):
+        self.edgeList.append(edge)
+    
+    def getEdgeList(self):
+        return self.edgeList
     
