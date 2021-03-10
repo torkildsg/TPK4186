e5f0e0756d7ac9276c20140417ef1cb5c4e492b2
torkilsg <torkilsg@stud.ntnu.no>
2021-03-10 15:53:03 +0100

    generator.py added

---
+++ assignment2/Generator.py
@@ -0,0 +1,10 @@
+"""" Group 14: Eivind Stangebye Larsen and Torkild Sandnes Grøstad """
+
+from Graph import Graph
+from Edge import Edge
+from Node import Node
+from Calculator import Calculator
+
+class Generator:
+
+    
