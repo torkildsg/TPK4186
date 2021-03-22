0a95b74c1005fc960060278aa45100df2ce0eba6
torkilsg <torkilsg@stud.ntnu.no>
2021-03-22 13:22:07 +0100

    added files

--- assignment3/Batch.py
+++ assignment3/Batch.py
@@ -1,1 +1,18 @@
+"""" Group 14: Eivind Stangebye Larsen and Torkild Sandnes Grøstad """
 
+class Batch:
+
+    def __init__(self, bufferCode, numOfWafers):
+        self.bufferCode = bufferCode
+        self.numOfWafers = numOfWafers
+    
+    def getBufferCode(self):
+        return self.bufferCode
+    
+    def setNumOfWafers(self, num):
+        self.numOfWafers = num
+    
+    def getNumOfWafers(self):
+        return self.numOfWafers
+    
+    
