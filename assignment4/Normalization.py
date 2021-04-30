35171ef75ee5e419549ae7ad2a0a082b591128ff
eivind <eivinsla@stud.ntnu.no>
2021-04-30 14:46:09 +0200

    task

--- assignment4/Normalization.py
+++ assignment4/Normalization.py
@@ -76,34 +76,6 @@ class Normalization:
             return file"""
   
 
-    """def importProject(self, csv_file):
-        try:
-            inputFile = open(csv_file, 'r')
-        except FileNotFoundError:
-            print("The file " + csv_file + "does not exist.")
-    
-        #array = np.genfromtxt(islice(inputFile, 3, 5))
-        content = [x.strip() for x in inputFile.readlines()] 
-        newContent = []
-        headers = []
-        week = 0
-
-        for i in range(len(content)):
-            thisLine = content[i].split('\t')
-            if i == 0:
-                self.setProjectName(thisLine[1])
-            elif i == 1:
-                self.setExpectedDuration(thisLine[1])
-            elif i == 2:
-                headers = thisLine
-            else: 
-                floatLine = []
-                for e in thisLine:
-                    floatLine.append(float(e))
-                week += 1
-                newContent.append(floatLine)
-            self.setAcutalDuration(week)
-        return newContent"""
 
 
 """ Testing """
