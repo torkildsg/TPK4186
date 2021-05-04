668abb2e8ae03daf43809cf71042c74e8b259316
torkilsg <torkilsg@stud.ntnu.no>
2021-05-04 16:12:49 +0200

    finishing

--- assignment4/Tester.py
+++ assignment4/Tester.py
@@ -6,50 +6,55 @@ from ProjectPrediction import ProjectPrediction
 """ Testing Task1 """
 
 path = "projectData"
-
 in_Normalization = Normalization()
-dictOfAllProjects = in_Normalization.readFiles(path)
-#in_Normalization.plotHistorgramOfDelays()
+
+"""dictOfAllProjects = in_Normalization.readFiles(path) # read all files in this folder
+in_Normalization.plotHistorgramOfDelays()"""
 
 #------------------------------------------------------------------------------------------------------------
 
 """ Testing Task 2 """
-# All projects that is a fiasco: 15, 28, 40, 47, 60, 76
-# Here you can decide the # of weeks you want to use (the number is a decimal)
+"""
+# Here you can decide the # of weeks (in a precentage) you want to use (the number has to be a decimal)
 classificationDf = in_Normalization.generateDataFrameForClassification(dictOfAllProjects, 0.2) 
 predict = ProjectPrediction()
 
 # Here you can print put the dataframe to see how it looks like
 print(classificationDf)
 
-""" Testing Logistic Regression """
-# Here you can decide the test-size (in decimal)
-#predict.logisticReg(classificationDf, 0.2)
+# In all three algorithms you can decide the test-split (in decimal)
 
-""" Testing KNeighbors """
-#predict.KNeighbors(classificationDf, 0.2)
+# Logistic Regression 
+predict.logisticReg(classificationDf, 0.2)
 
-""" Testing Naive Bayes """
-#predict.naiveBayes(classificationDf, 0.2)
+# KNeighbors 
+predict.KNeighbors(classificationDf, 0.2)
 
-""" Testing Decision Tree """
-#predict.decisionTree(classificationDf, 0.20)
+# Naive Bayes 
+predict.naiveBayes(classificationDf, 0.2)
+
+# Decision Tree 
+predict.decisionTree(classificationDf, 0.2)
+"""
 
 #------------------------------------------------------------------------------------------------------------
 
 """ Testing Task 3 """
+"""
 # Here you can decide the # of weeks you want to use (the number is a decimal)
 regressionDf = in_Normalization.generateDataFrameForRegression(dictOfAllProjects, 0.2)
 
 # Here you can print put the dataframe to see how it looks like
-#print(regressionDf)
+print(regressionDf)
 
-""" Testing Linear SVR """
-# Here you can decide the test-split (in decimal)
-#predict.SVRlinear(regressionDf, 0.2)
+# In all three algorithms you can decide the test-split (in decimal)
 
-""" Testing Linear Lasso """
-#predict.LassoLinear(regressionDf, 0.2)
+#Linear SVR 
+predict.SVRlinear(in_Normalization, regressionDf, 0.2)
 
-""" Testing Ridge Regression """
-#predict.ridgeReg(regressionDf, 0.2)
+# Linear Lasso 
+predict.LassoLinear(in_Normalization, regressionDf, 0.2)
+
+# Ridge Regression 
+predict.ridgeReg(in_Normalization, regressionDf, 0.2)
+"""
